98a58bc57c5c4b2fb8770bc204441c62dc33d016
Johannes Wehner <johsw@information.dk>
2015-02-22 21:46:05 +0100

    Fixed domain in service.

--- frontend/src/app/services/latest.js
+++ frontend/src/app/services/latest.js
@@ -1,7 +1,6 @@
 'use strict';
 
 app.factory('Latest', function($resource, BACKEND_ADDRESS) {
-    
-    return $resource('http://www.stiften.dk/mecommobile/latest/:type/:id/:items?output_type=json&mmfd_version=2.0&show_related=true&show_external=true&show_external_sources=true&image_size=866x487-c');
+    return $resource('http://stiften.dk/mecommobile/latest/:type/:id/:items?output_type=json&mmfd_version=2.0&show_related=true&show_external=true&show_external_sources=true&image_size=866x487-c');
 });
 
