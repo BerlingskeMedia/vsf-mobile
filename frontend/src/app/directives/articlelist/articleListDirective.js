095f7752daacf5febd84f18dc633787376f0a379
Johannes Wehner <johsw@information.dk>
2015-02-22 21:47:05 +0100

    Abstracted list directives.

--- frontend/src/app/directives/articlelist/articleListDirective.js
+++ frontend/src/app/directives/articlelist/articleListDirective.js
@@ -1,32 +1,46 @@
 'use strict';
 
-app.directive('stiftenImageArticleList', function() {
+app.directive('stiftenArticleList', function() {
     return {
         restrict: 'AEC',
-        templateUrl: 'app/directives/articlelist/imageArticleListTemplate.html',
         scope: true,
-        controller: function($scope, $attrs, ForsideContent, $location) {
-          
-          /*
-          if ($attrs.content = 'frontpageFeatured') {
-            $scope.article = $rootScope.frontpageArticles.items;
-          }*/
-          
-          if ($attrs.content = 'otherFrontpageArticles') {
-            //articles = $rootScope.frontpageArticles.items;
-            //articles.shift();
-           
-            var frontpageContent = ForsideContent.get();
-            frontpageContent.$promise.then(function(){
-              $scope.articles = frontpageContent.items;
+        controller: function($scope, $attrs, Nodequeue, Latest, $location) {
+
+          if ($attrs.content == 'latest') {
+            var latest =  Latest.get({id:$attrs.id, items:$attrs.items, type:$attrs.type});
+            latest.$promise.then(function(){
+              console.log(latest.items);
+              $scope.articles = latest.items;
             });
-            
+          }
+
+          if ($attrs.content == 'nodequeue') {
+            var nodequeue =  Nodequeue.get({id:$attrs.id, items:$attrs.items});
+            nodequeue.$promise.then(function(){
+              $scope.articles = nodequeue.items;
+            });
           }
 
           $scope.showArticle = function(guid) {
             $location.path('/path/' + guid);
             console.log(guid);
-          }  
+          }
         }
+    };
+});
+
+app.directive('stiftenImageArticleList', function() {
+    return {
+        restrict: 'AEC',
+        templateUrl: 'app/directives/articlelist/imageArticleListTemplate.html',
+        scope: true
+    };
+});
+
+app.directive('stiftenHeaderArticleList', function() {
+    return {
+        restrict: 'AEC',
+        templateUrl: 'app/directives/articlelist/headerArticleListTemplate.html',
+        scope: true
     };
 });
