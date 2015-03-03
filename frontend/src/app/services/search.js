7a15428b6dbc1455857a6f7c5c4878a2f2452d62
Johannes Wehner <johsw@information.dk>
2015-03-03 13:35:25 +0100

    Search service, made relative. Needs routing outside of the app.

--- frontend/src/app/services/search.js
+++ frontend/src/app/services/search.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app.factory('Search', function($resource, BACKEND_ADDRESS) {
-    //return $resource('http://search.berlingskemedia.net/gateway/Search.json?source_1_name=stiften.dk&query=:query');
+    // Using realtive url, but it needs to be routed to http://search.berlingskemedia.net
     return $resource('/gateway/Search.json?source_1_name=stiften.dk&source_1_sort_by=date&source_1_rpp=5&source_1_page=:page&query=:query');
 });
 
