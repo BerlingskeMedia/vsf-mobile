05e7fee65e5045f7d1943804e51b912d08d193e6
Johannes Wehner <johsw@information.dk>
2015-03-04 21:05:03 +0100

    A horrible temp hack to make category links work.

--- frontend/src/app/pages/listPage/listPageController.js
+++ frontend/src/app/pages/listPage/listPageController.js
@@ -3,10 +3,18 @@
 'use strict';
 
 app.controller('ListController', function ($scope, $rootScope, $routeParams, Category, Latest) {
+    // We need to lookup term-ids based on slugs in BOND - this is an UGLY  hack.
   var id = 0;
   if ('tag' in $routeParams) {
-     id = Category($routeParams.tag);
+    if (isNaN(parseFloat($routeParams.tag))) {
+      id = Category($routeParams.tag);
+    } else {
+      id = $routeParams.tag;
+    }
+    
+
   }
+  
   var latest =  Latest.get({id:id, items: 20, type: 'news_article'});
   latest.$promise.then(function(){
     $scope.header = latest.category;
