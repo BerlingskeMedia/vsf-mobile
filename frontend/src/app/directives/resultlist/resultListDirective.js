948862179fb7cb9146f40473881d6157ccb00d5e
Johannes Wehner <johsw@information.dk>
2015-03-05 09:42:49 +0100

    Add image-conditional class

--- frontend/src/app/directives/resultlist/resultListDirective.js
+++ frontend/src/app/directives/resultlist/resultListDirective.js
@@ -5,12 +5,5 @@ app.directive('stiftenResultList', function() {
         restrict: 'AEC',
         scope: true,
         templateUrl: 'app/directives/resultlist/searchResultArticleListTemplate.html',
-        controller: function($scope, $location) {
-          $scope.showArticle = function(article) {
-            var absoluteUrl = article.link;
-            var relativeUrl = absoluteUrl.replace('http://stiften.dk', '');
-            $location.path(relativeUrl);
-          }
-        }
     };
 });
