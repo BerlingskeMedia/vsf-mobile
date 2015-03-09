c8a88dc09a5e35ce82001635a07a411dc7cb753b
Johannes Wehner <johsw@information.dk>
2015-03-09 15:19:17 +0100

    Testing out embedded links

--- frontend/src/app/services/contentitem.js
+++ frontend/src/app/services/contentitem.js
@@ -1,9 +1,9 @@
 'use strict';
 
 app.factory('ContentItemById', function($resource, BACKEND_ADDRESS) {
-    return $resource(BACKEND_ADDRESS + '/mecommobile/node/:id');
+    return $resource(BACKEND_ADDRESS + '/mecommobile/node/:id?show_embedded=true&field_keys=true');
 });
 
 app.factory('ContentItemByPath', function($resource, BACKEND_ADDRESS, DOMAIN) {
-    return $resource(BACKEND_ADDRESS + '/mecommobile/node/' + DOMAIN + '/:tag/:id');
+    return $resource(BACKEND_ADDRESS + '/mecommobile/node/' + DOMAIN + '/:tag/:id?show_embedded=true&field_keys=true');
 });
