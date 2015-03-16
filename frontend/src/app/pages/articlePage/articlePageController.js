30c8d22ccd1389c56d1ee5b58fcb3673344bacee
Johannes Wehner <johsw@information.dk>
2015-03-16 21:11:09 +0100

    Localstorage for articles.

--- frontend/src/app/pages/articlePage/articlePageController.js
+++ frontend/src/app/pages/articlePage/articlePageController.js
@@ -2,12 +2,19 @@
 
 'use strict';
 
-app.controller('ArticleController', function ($scope, $rootScope, ContentItemByPath, DOMAIN, $routeParams) {
+app.controller('ArticleController', function ($scope, $rootScope, ContentItemByPath, DOMAIN, $routeParams, localStorageService) {
 
   $rootScope.pageTypeClass = 'page-article-page';
+
+  var article = localStorageService.get($routeParams.tag + '/' + $routeParams.articleid);
+  if (article !== null) {
+    $scope.article = article;
+  }
+
   var content =  ContentItemByPath.get({tag:$routeParams.tag, id:$routeParams.articleid});
   content.$promise.then(function(){
     $scope.article = content.items[0];
+    localStorageService.set($routeParams.tag + '/' + $routeParams.articleid, $scope.article);
   });
 
 
