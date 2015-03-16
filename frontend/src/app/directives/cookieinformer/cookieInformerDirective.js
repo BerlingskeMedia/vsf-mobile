08d6c8b300731d9f86238a0355448a088caab6e9
Johannes Wehner <johsw@information.dk>
2015-03-16 12:48:42 +0100

    Cookie consent.

--- frontend/src/app/directives/cookieinformer/cookieInformerDirective.js
+++ frontend/src/app/directives/cookieinformer/cookieInformerDirective.js
@@ -6,17 +6,18 @@ app.directive('cookieinformer', function(){
         templateUrl: 'app/directives/cookieinformer/cookieInformerTemplate.html',
         $scope: false,
         controller : function($scope, $cookies) {
-	        
-	        // Checking if user already clicked the OK button
-	        if($cookies.informed) {
-		        $scope.statusClass = 'done';
-	        }
-	        
-	        // Handling click on the OK button and hiding the bookmarklet
-	        $scope.closeInformer = function() {
-		        $scope.statusClass = 'done';
-		        document.cookie = 'informed=true; expires=Fri, 3 Aug 2200 00:00:00 UTC; path=/'
-            }
+          
+          $scope.showCookieInformer = false;
+          // Checking if user already clicked the OK button
+          if(!('informed' in $cookies)) {
+            $scope.showCookieInformer = true;
+          }
+
+          // Handling click on the OK button and hiding the element
+          $scope.closeInformer = function() {
+            document.cookie = 'informed=true; expires=Fri, 3 Aug 2200 00:00:00 UTC; path=/';
+            $scope.showCookieInformer = false;
+          }
         }
     };
 });
