2d4c1effd39c7cb6a71fe89f431b1d00aaae03a1
Johannes Wehner <johsw@information.dk>
2015-03-23 11:06:17 +0100

    Alert now has real data.
    Two alerts are shown now for testing purposes. Should be one later

--- frontend/src/app/directives/newsalert/newsAlertDirective.js
+++ frontend/src/app/directives/newsalert/newsAlertDirective.js
@@ -4,13 +4,53 @@ app.directive('stiftenNewsAlert', function(){
     return {
         restrict: 'AEC',
         templateUrl: 'app/directives/newsalert/newsAlertTemplate.html',
-        controller : function($scope) {
+        controller : function($scope, localStorageService, Nodequeue) {
           $scope.displayAlert = false;
           $scope.alertVisibility = 'alert-displayed';
-          $scope.alertLabel = 'Alarm';
-          $scope.alertTitle = 'Test alert tekst';
-          $scope.alertType = 'alert-breaking';
-          $scope.alertUrl = 'http://stiften.dk';
+          var alertArticles = localStorageService.get('alertArticles');
+          if (alertArticles !== null) {
+            $scope.alertArticles = alertArticles;
+          }
+
+          var nodequeue =  Nodequeue.get({id:5910, items:2});
+          nodequeue.$promise.then(function(){
+            // If the breaking queu is not empty we update.
+            if (nodequeue.items.length != 0) {
+
+              // Here we traverse to extract presentation-tags.
+              for (var i=0; i < nodequeue.items.length; i++) {
+                for (var x=0; x < nodequeue.items[i].fields.length; x++) {
+                  if (nodequeue.items[i].fields[x].attributes.keys == 'p_tag') {
+                    if (nodequeue.items[i].fields[x].attributes.label == 328917) {
+
+                      nodequeue.items[i].alertType = 'alert-breaking';
+                      nodequeue.items[i].alertLabel = 'Breaking';
+                    }
+                    if (nodequeue.items[i].fields[x].attributes.label == 328920) {
+
+                      nodequeue.items[i].alertType = 'alert-justnow';
+                      nodequeue.items[i].alertLabel = 'Netop nu';
+                    }
+                  }
+                }
+              }
+
+              $scope.alertArticles = nodequeue.items;
+              localStorageService.set('alertArticles', $scope.alertArticles);
+
+
+            // If its empty we just clear local storage.
+            // so articles will be removed on next load
+            } else {
+
+              localStorageService.set('alertArticles', $scope.alertArticles);
+            }
+
+          });
+
+
+
+
         }
     };
 });
