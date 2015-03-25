1ac9d357b9bfe2bc2b29f2590b5c23e85f76e3c1
Johannes Wehner <johsw@information.dk>
2015-03-25 10:09:46 +0100

    Refactored cookie-stuff.

--- frontend/src/app/directives/cookieinformer/cookieInformerDirective.js
+++ frontend/src/app/directives/cookieinformer/cookieInformerDirective.js
@@ -6,17 +6,17 @@ app.directive('cookieinformer', function(){
         templateUrl: 'app/directives/cookieinformer/cookieInformerTemplate.html',
         $scope: false,
         controller : function($scope, $cookies) {
-          
-          $scope.showCookieInformer = false;
+
+          $scope.cookieConsentVisibility = '';
           // Checking if user already clicked the OK button
           if(!('informed' in $cookies)) {
-            $scope.showCookieInformer = true;
+            $scope.cookieConsentVisibility = 'cookie-consent-visible';
           }
 
           // Handling click on the OK button and hiding the element
           $scope.closeInformer = function() {
             document.cookie = 'informed=true; expires=Fri, 3 Aug 2200 00:00:00 UTC; path=/';
-            $scope.showCookieInformer = false;
+            $scope.cookieConsentVisibility = false;
           }
         }
     };
