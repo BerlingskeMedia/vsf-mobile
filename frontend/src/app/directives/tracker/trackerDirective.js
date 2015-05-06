97d077b4ef72e7f174d6de0702133c36d7e91695
Johannes Wehner <johsw@information.dk>
2015-05-06 15:12:41 +0200

    TRacking accounts.

--- frontend/src/app/directives/tracker/trackerDirective.js
+++ frontend/src/app/directives/tracker/trackerDirective.js
@@ -1,6 +1,6 @@
 /*jslint nomen: true*/
 /*global udm_, ga, pp_gemius_event, pp_gemius_identifier, _sf_async_config*/
-app.directive('berlTracking', function ($rootScope, $location, $routeParams) {
+app.directive('berlTracking', function ($rootScope, $location, $routeParams, config) {
     'use strict';
 
     return {
@@ -10,7 +10,7 @@ app.directive('berlTracking', function ($rootScope, $location, $routeParams) {
             var track = function () {
 
                 // Comscore
-                udm_('//int.sitestat.com/berlingske/bite/s?<SectionName>.panel');
+                udm_('//int.sitestat.com/berlingske/m-aas/s?<SectionName>.panel');
 
                 // Facebookththth
                 //window._fbq = window._fbq || [];
