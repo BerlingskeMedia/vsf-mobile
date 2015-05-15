d5f1f402fbd117d4f6f650c32ae07ada31e3a2dd
Johannes Wehner <johsw@information.dk>
2015-05-15 09:14:34 +0200

    Use v2 of the mecommobile api.

--- frontend/src/app/services/contentitem.js
+++ frontend/src/app/services/contentitem.js
@@ -1,9 +1,9 @@
 'use strict';
 
 app.factory('ContentItemById', function($resource, BACKEND_ADDRESS) {
-    return $resource(BACKEND_ADDRESS + '/mecommobile/node/:id?show_embedded=true&field_keys=true&output_type=json&image_size=:imagesize&show_video');
+    return $resource(BACKEND_ADDRESS + '/mecommobile/node/:id?mmfd_version=2.0&show_embedded=true&field_keys=true&output_type=json&image_size=:imagesize&show_video');
 });
 
 app.factory('ContentItemByPath', function($resource, BACKEND_ADDRESS, DOMAIN) {
-    return $resource(BACKEND_ADDRESS + '/mecommobile/node/' + DOMAIN + '/:tag/:id?show_embedded=true&field_keys=true&output_type=json&image_size=:imagesize&show_video');
+    return $resource(BACKEND_ADDRESS + '/mecommobile/node/' + DOMAIN + '/:tag/:id?mmfd_version=2.0&show_embedded=true&field_keys=true&output_type=json&image_size=:imagesize&show_video');
 });
