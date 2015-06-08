a8fd0934c690ad0083f56fc0344a089e6f23efbb
Johannes Wehner <johsw@information.dk>
2015-06-08 21:38:29 +0200

    Add some "load more"'s + remove warning.

--- frontend/src/app/directives/gallery/galleryDirective.js
+++ frontend/src/app/directives/gallery/galleryDirective.js
@@ -6,35 +6,37 @@ app.directive('stiftenGallery', function(){
         templateUrl: 'app/directives/gallery/galleryTemplate.html',
 
         controller: function ($scope, $window, $timeout) {
-            $scope.images = $scope.story.media.image;
-            $scope.listenerAttached = false;
-            $scope.currentSlide = 0;
-            $scope.showGalleryOverlay = false;
-            $scope.toggleGalleryOverlay = function() {
-                $scope.showGalleryOverlay = !$scope.showGalleryOverlay;
-                if ($scope.showGalleryOverlay) {
-                  $scope.flipsnap.element.style.width = ($scope.images.length * 100) + 'vw';
-                  $timeout(function(){
-                      $scope.flipsnap.refresh();
-                  }, 500);
-                  if (!$scope.listenerAttached) {
-
-                      $scope.listenerAttached = true;
-                      var el = $scope.flipsnap.element;
-                      el.addEventListener('fstouchend', function(ev) {
-                          $scope.currentSlide = ev.newPoint;
-                      }, false);
-
-                      angular.element($window).bind('resize', function() {
-                          $scope.flipsnap.element.style.width = ($scope.images.length * 100) + 'vw';
+            if ('media' in $scope.story && 'image' in $scope.story.media) {
+                $scope.images = $scope.story.media.image;
+                $scope.listenerAttached = false;
+                $scope.currentSlide = 0;
+                $scope.showGalleryOverlay = false;
+                $scope.toggleGalleryOverlay = function() {
+                    $scope.showGalleryOverlay = !$scope.showGalleryOverlay;
+                    if ($scope.showGalleryOverlay) {
+                      $scope.flipsnap.element.style.width = ($scope.images.length * 100) + 'vw';
+                      $timeout(function(){
                           $scope.flipsnap.refresh();
-                      });
-                      $scope.setSlide = function(id) {
-                          $scope.currentSlide = id;
-                          $scope.flipsnap.moveToPoint(id);
+                      }, 500);
+                      if (!$scope.listenerAttached) {
 
+                          $scope.listenerAttached = true;
+                          var el = $scope.flipsnap.element;
+                          el.addEventListener('fstouchend', function(ev) {
+                              $scope.currentSlide = ev.newPoint;
+                          }, false);
+
+                          angular.element($window).bind('resize', function() {
+                              $scope.flipsnap.element.style.width = ($scope.images.length * 100) + 'vw';
+                              $scope.flipsnap.refresh();
+                          });
+                          $scope.setSlide = function(id) {
+                              $scope.currentSlide = id;
+                              $scope.flipsnap.moveToPoint(id);
+
+                          }
                       }
-                  }
+                    }
                 }
             }
 
