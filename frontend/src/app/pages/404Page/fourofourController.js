001211578797eaa3fbf39e0fbca62e1a91c69c1a
Johannes Wehner <johsw@information.dk>
2015-06-19 14:57:53 +0200

    Baseline 404

--- frontend/src/app/pages/404Page/fourofourController.js
+++ frontend/src/app/pages/404Page/fourofourController.js
@@ -1,11 +1,6 @@
 'use strict';
 
-app.controller('fourofourController', function($routeParams, $scope, $location, config, ContentItems) {
-    var params = {extendfirstitems: 'no', extendtemplates: 'yes', filter : 'tiles'};
-
-    params.pagesize = 10;
-    params.sortby = 'published';
-
-    $scope.articles = ContentItems.get(params);
+app.controller('fourofourController', function($rootScope, config) {
+    $rootScope.pageTypeClass = 'page-404-page';
     $rootScope.$emit('tracking');
 });
