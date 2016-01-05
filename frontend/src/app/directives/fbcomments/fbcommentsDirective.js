18050dc63138a66246dafe6db1e47224aac52c0d
Johannes Wehner <johsw@information.dk>
2016-01-05 11:22:07 +0100

    Small corrections to avoid log warnings.

--- frontend/src/app/directives/fbcomments/fbcommentsDirective.js
+++ frontend/src/app/directives/fbcomments/fbcommentsDirective.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.directive('stiftenFbcomments', function($location){
+app.directive('stiftenFbcomments', function(){
     function createHTML(href, numposts, colorscheme) {
          return '<div class="fb-comments" ' +
                         'data-width="100%" ' +
@@ -13,16 +13,17 @@ app.directive('stiftenFbcomments', function($location){
      return {
          restrict: 'A',
          scope: {},
-         link: function postLink(scope, elem, attrs) {
+         controller: function($location, $element) {
 
 
              var href        = $location.absUrl();
              var numposts    = 5;
              var colorscheme = 'light';
 
-             elem.html(createHTML(href, numposts, colorscheme));
-             FB.XFBML.parse(elem[0]);
-
+             $element.html(createHTML(href, numposts, colorscheme));
+             if (typeof(FB) !== 'undefined') {
+               FB.XFBML.parse($element[0]);
+             }
          }
      };
 });
