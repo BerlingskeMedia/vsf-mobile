8a7cca8e52394df5c45292f745679f72ae122ec1
Johannes Wehner <johsw@information.dk>
2016-01-05 09:56:33 +0100

    Add auth to weather.

--- frontend/src/app/services/weather.js
+++ frontend/src/app/services/weather.js
@@ -2,7 +2,7 @@
 
 app.factory('Weather', function($resource) {
     // Using realtive url, but it needs to be routed to http://search.berlingskemedia.net
-    return $resource('http://api.openweathermap.org/data/2.5/forecast?q=:city&mode=json&units=metric');
+    return $resource('http://api.openweathermap.org/data/2.5/forecast?q=:city&mode=json&units=metric&APPID=ac022275fd9d4d7886929e7db1cdddaf');
 });
 
 
