b8a9091ee7bbe0b5c959b10b2777171174797f5a
Johannes Wehner <johsw@information.dk>
2016-01-18 20:53:01 +0100

    Initial markup for premium.

---
+++ frontend/src/app/directives/premium/premiumDirective.js
@@ -0,0 +1,8 @@
+'use strict';
+
+app.directive('stiftenPremium', function(){
+    return {
+        restrict: 'AEC',
+        templateUrl: 'app/directives/premium/premiumTemplate.html',
+    };
+});
