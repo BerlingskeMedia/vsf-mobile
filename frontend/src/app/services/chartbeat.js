118695d240ce074be47d73c5345744e83731fe4e
Johannes Wehner <johsw@information.dk>
2016-02-04 10:37:18 +0100

    Proxy chartbeat urls'

--- frontend/src/app/services/chartbeat.js
+++ frontend/src/app/services/chartbeat.js
@@ -1,6 +1,6 @@
 'use strict';
 
 app.factory('Chartbeat', function($resource, config) {
-  return $resource('http://api.chartbeat.com/live/toppages/v3/?apikey=' + config.chartbeatApikey + '&host=stiften.dk&types=1&limit=20');
+  return $resource('http://chartbeat-proxy.bemit.dk/live/toppages/v3/?apikey=' + config.chartbeatApikey + '&host=stiften.dk&types=1&limit=20');
 });
 
