6122a19cf909a8b111b3eb90c62164bf9e7a966a
Johannes Wehner <johsw@information.dk>
2016-03-02 10:28:10 +0100

    Inject backend adress

--- frontend/src/app/services/youtubeoembed.js
+++ frontend/src/app/services/youtubeoembed.js
@@ -1,9 +1,7 @@
 'use strict';
 
-app.factory('YoutubeoEmbed', function($resource) {
-
+app.factory('YoutubeoEmbed', function($resource, BACKEND_ADDRESS) {
     return $resource(BACKEND_ADDRESS + '/yt_oembed?id=:youtubeid');
-
 });
 
 
