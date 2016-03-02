a1ba14687e2e8f520f1b8fd02e8788ae4f55d7ad
Johannes Wehner <johsw@information.dk>
2016-03-02 10:00:33 +0100

    VideoLink comments.

--- frontend/src/app/directives/videolink/videoLinkDirective.js
+++ frontend/src/app/directives/videolink/videoLinkDirective.js
@@ -6,11 +6,10 @@ app.directive('stiftenVideoLink', function(){
         templateUrl: 'app/directives/videolink/videoLinkTemplate.html',
         controller: function ($scope, YoutubeoEmbed,$sce) {
           if ($scope.story.youtube_video_id) {
-            
-            //$scope.url = $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + $scope.story.youtube_video_id);
+            // Youtube videos just have an id, so we use a proxied oEmbed to get dimensions and iframe html.
+            // It's proxied to avoid CORS-issues.
             var data = YoutubeoEmbed.get({ youtubeid: $scope.story.youtube_video_id });
             data.$promise.then(function(data){
-              console.log(data);
               $scope.video = $sce.trustAsHtml(data.html);
             });
           }
